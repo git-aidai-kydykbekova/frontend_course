54edf4a096b4334e84af571f8e658664fa3dce6c
git-aidai-kydykbekova <aidai.kydykbekova@alatoo.edu.kg>
2024-11-29 21:44:37 +0600

    initialized array using useEffect

--- filter/app.js
+++ filter/app.js
@@ -1,9 +1,29 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
     const [data, setData] = useState([]);
     const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            const result = [
+                { id: 1, name: 'Math' },
+                { id: 2, name: 'Biology' },
+                { id: 3, name: 'Chemistry' },
+                { id: 4, name: 'History' },
+                { id: 5, name: 'English' },
+                { id: 6, name: 'Geometry' },
+                { id: 7, name: 'Reading' },
+                { id: 8, name: 'Art' },
+                { id: 9, name: 'PE' },
+                { id: 10, name: 'Break' },
+            ];
+            setData(result);
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div>
