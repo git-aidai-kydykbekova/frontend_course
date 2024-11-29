c68b963121fef2f5322754639ea12defe785b42a
git-aidai-kydykbekova <aidai.kydykbekova@alatoo.edu.kg>
2024-11-29 21:42:16 +0600

    create state for saving data and for searching

---
+++ filter/app.js
@@ -0,0 +1,15 @@
+
+import React, { useState } from 'react';
+
+function App() {
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState('');
+
+    return (
+        <div>
+            <h1>Data Filter</h1>
+        </div>
+    );
+}
+
+export default App;
